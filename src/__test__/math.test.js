3e9aecef320eba9c91bb55eefee43b535ef7d700
toonneedmoneyyyy <punnapa.tri@dome.tu.ac.th>
2024-10-15 10:42:00 +0700

    Initial commit

---
+++ src/__test__/math.test.js
@@ -0,0 +1,17 @@
+const { add, subtract, multiply } = require('./math');
+
+describe('simple arithmetic', () => {
+
+    test('addition', () => {
+        expect(add(5, 3)).toBe(8);
+    });
+
+    test('subtract', () => {
+        expect(subtract(5, 1)).toBe(4);
+    });
+
+    test('multiply', () => {
+        expect(multiply(2, 3)).toBe(6);
+    });
+
+});
